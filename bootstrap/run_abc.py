1191a133f0a1a8fc41f4685b223cf8de4ab9ef46
Oliver Xu <oliver.xu@arup.com>
2022-09-15 00:17:23 +1000

    fix algo

--- bootstrap/run_abc.py
+++ bootstrap/run_abc.py
@@ -10,7 +10,10 @@ green_y = data['green_y']
 green_z = data['green_z']
 
 target_drop_ratio = blue_x.shape[0]/green_y.shape[0]
+target_mean_drop_height = np.concatenate((blue_z, green_z)).mean()
+
 tol_drop_ratio = 0.1
+tol_mean_drop_height = 0.02
 
 N_drops = 10000
 N_expts = 2000
@@ -28,7 +31,7 @@ for i in range(N_expts):
 
     # Test ABC criteria
     criteria1 = np.abs(target_drop_ratio - drop_ratio) < tol_drop_ratio
-    criteria2 = np.abs(target_drop_ratio - mean_drop_height) < tol_drop_ratio
+    criteria2 = np.abs(target_mean_drop_height - mean_drop_height) < tol_mean_drop_height
     if criteria1 and criteria2:
         if accepted is None:
             accepted = np.array([x ,y])
@@ -37,5 +40,4 @@ for i in range(N_expts):
 plt.scatter(accepted[:, 0], accepted[:, 1], color='none', edgecolor='black')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.savefig('abc_criteria1.png')
-
+plt.savefig('abc_criteria2.png')
