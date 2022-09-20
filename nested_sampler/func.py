d8f73163e8e8a3789104e74d9b0a2deb63dec916
Oliver Xu <oliver.xu@arup.com>
2022-09-20 22:35:01 +1000

    import axes3D

--- nested_sampler/func.py
+++ nested_sampler/func.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def likelihood(x, y):
     return -1/5428 * (np.sin(x) * np.exp((1 - np.cos(y))**2) + np.cos(y) * \
