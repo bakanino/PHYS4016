3b74d04ae773bda54cb505daa2f1675a12ff5397
Oliver Xu <oliver.xu@arup.com>
2022-09-29 23:50:42 +1000

    exp for time

--- galaxies/sol/exp.py
+++ galaxies/sol/exp.py
@@ -3,4 +3,7 @@ import math
 def exp(mu, lmbda):
     return math.e**(-lmbda/mu)
 
-print(0.6/(exp(20, 60)*0.4 + 0.6))
+#print(0.6/(exp(20, 60)*0.4 + 0.6))
+
+X = -math.log((2/3-0.6)/0.4,math.e)*20
+print(X)
