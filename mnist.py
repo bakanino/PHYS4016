2a96fec268a6d0254c2c6a0d4428fafe447ff6bf
Oliver Xu <oliver.xu@arup.com>
2022-10-21 13:28:17 +1100

    show errored test points

--- mnist.py
+++ mnist.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import keras
 from keras.layers import InputLayer, Dense
 import numpy as np
@@ -25,8 +26,25 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+history = model.fit(x_train, y_train, epochs=10)
+#
+#loss, accuracy = model.evaluate(x_test, y_test)
+#
+#print(accuracy)
+#0.9864652156829834
 
-loss, accuracy = model.evaluate(x_test, y_test)
-
-print(accuracy) #0.9836323857307434
+predictions = model.predict(x_test)
+examples = 100
+print('Predictions:', np.argmax(predictions[:examples], axis=1))
+print('Ground truth:', np.argmax(y_test[:examples], axis=1))
+print('Equality test:', np.argmax(predictions[:examples], axis=1) == np.argmax(y_test[:examples], axis=1))
+for i in range(x_test.shape[0]):
+    predicted = np.argmax(predictions[i])
+    truth = np.argmax(y_test[i])
+    if predicted != truth:
+        print('Wrong prediction on test point', i)
+        plt.figure()
+        plt.imshow(x_test[i].reshape((28, 28)), cmap='binary')
+        plt.title('Predicted = {}, Truth = {}'.format(predicted+1, truth+1))
+        plt.savefig('im/Test point {}.png'.format(i))
+        plt.close()
